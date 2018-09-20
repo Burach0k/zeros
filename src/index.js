d9e73d70b409a2c32df5c09c60eb4738fdcbdc1e
Burach0k <Burak_andre@mail.ru>
2018-09-21 02:11:09 +0300

    3

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 module.exports = function getZerosCount(number) {
 var mas = [];
-var f = 0;
+var f = number;
+var s = 0;
+var a;
   for(var i = 1; i <= Math.floor(number/5); i++){
-    mas[i] = i*5;
-  }
-  for(var k = 0; k<mas.length; k++){
-    while(mas[k]%5 == 0){
-      mas[k] = mas[k]/5;
-      f++;
+    a = i*5;
+    while(a%5 == 0){
+      a = a/5;
+      s++;
     }
   }
-  return f;
+  return s;
 }
